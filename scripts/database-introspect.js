07de0958e02dd781fc81073b7de6085a5e194b5d
armando <armandomvillanueva@icloud.com>
2023-06-28 10:45:09 -0700

    feat: Introspect ingore script

--- scripts/database-introspect.js
+++ scripts/database-introspect.js
@@ -1,13 +1,8 @@
-
-generator client {
-  provider = "prisma-client-js"
-}
-
-datasource db {
-  provider = "postgresql"
-  url      = env("DATABASE_URL")
-}
-
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const fs = require('fs');
+const MODEL_REGEX = /model [a-zA-Z0-9_]+ {[^}]*}/g;
+const SYNC_NODE_MODELS = `
 
 model asks {
     id                      Bytes   @id @db.ByteA
@@ -90,19 +85,34 @@ model asks {
     price_amount_usd     Decimal? @db.Decimal
     price_amount_native  Decimal? @db.Decimal
   }
+`;
 
+(() => {
+  const oldSchema = fs.readFileSync('prisma/schema.prisma', 'utf-8');
+
+  const ignoredModels = oldSchema.match(MODEL_REGEX).map((match) => {
+    const lines = match.split('\n');
+    if (!lines.some((str) => str.trim() === '@@ignore')) {
+      lines[lines.length - 1] = '\n@@ignore\n}';
+    }
+
+    return lines.join('\n');
+  });
+
+  const updatedSchema = `
+generator client {
+  provider = "prisma-client-js"
+}
+
+datasource db {
+  provider = "postgresql"
+  url      = env("DATABASE_URL")
+}
+${SYNC_NODE_MODELS}
 
 //THESE TABLES WERE INTROSPECTED BY PRISMA FROM THE DATABASE BUT ARE IGNORED ON THE CLIENT
-model test {
-  count Int?
+${ignoredModels.join('\n')}
+`;
 
-  @@ignore
-}
-model users {
-  userid    Int     @id
-  firstname String? @db.VarChar(255)
-  lastname  String? @db.VarChar(255)
-  email     String? @db.VarChar(255)
-
-@@ignore
-}
+  fs.writeFileSync('prisma/schema.prisma', updatedSchema, 'utf-8');
+})();
