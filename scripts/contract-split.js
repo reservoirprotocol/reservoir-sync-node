5dabd74d075b0d010a2761d13e9e5e1f2e4c0ed3
armando <armandomvillanueva@icloud.com>
2023-07-26 17:34:12 -0700

    feat: Contract reading stuff

--- scripts/contract-split.js
+++ scripts/contract-split.js
@@ -1,17 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-implicit-any-catch */
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const path = require('path');
 
-const contracts = [];
+try {
+  const contracts = [];
+  fs.readFileSync(path.join(__dirname, '../contracts.txt'), 'utf-8')
+    .trim()
+    .split('\n')
+    .map((contract) => contracts.push(contract));
 
-fs.readFileSync(path.join(__dirname, '../contracts.txt'), 'utf-8')
-  .trim()
-  .split('\n')
-  .map((contract) => contracts.push(contract));
-
-fs.writeFileSync(
-  path.join(__dirname, '../contracts.json'),
-  JSON.stringify(contracts),
-  'utf-8'
-);
+  fs.writeFileSync(
+    path.join(__dirname, '../contracts.json'),
+    JSON.stringify(contracts),
+    'utf-8'
+  );
+  console.log(`READ CONTRACTS`);
+  return;
+} catch (e) {
+  console.log(`ERROR READING CONTRACTS: ${e}`);
+  return;
+}
