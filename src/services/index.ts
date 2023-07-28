205357c43712ae319a9e41725b39172d7da9938f
armando <armandomvillanueva@icloud.com>
2023-07-28 10:31:06 -0700

    feat: Purge V2

--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './InsertionService';
-export * from './LoggerService';
-export * from './SyncManager';
-export * from './SyncService';
-export * from './SyncWorker';
-export * from './BackupService';
-export * from './WebSocketService';
