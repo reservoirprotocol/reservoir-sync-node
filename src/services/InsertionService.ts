afd31643c65e47042f49480b62e452e72863fd94
armando <armandomvillanueva@icloud.com>
2023-09-08 08:36:36 -0700

    Update InsertionService.ts

--- src/services/InsertionService.ts
+++ src/services/InsertionService.ts
@@ -120,14 +120,12 @@ class _InsertionService {
       const formatted = this._format(type, record);
       // @ts-ignore Prisma doesn't support model reference by variable name.
       // See https://github.com/prisma/prisma/discussions/16058#discussioncomment-5493
-      return await this._prisma[type].upsert({
+     await this._prisma[type].upsert({
         where: { id: formatted.id },
         create: formatted,
         update: formatted,
       });
     }
-
-    return;
   }
   /**
    * Filters the input data based on the available contracts.
